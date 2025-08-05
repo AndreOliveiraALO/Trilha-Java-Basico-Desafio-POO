554b1cad754b4346f3e78d4ba83a916c97d90fb2
André Luiz de Oliveira <andreloliveira28@gmail.com>
2025-08-04 23:55:38 -0300

    Rotina testando rotinas da classe IPhone

--- DesafioPOO/src/App.java
+++ DesafioPOO/src/App.java
@@ -1,5 +1,28 @@
+import entities.IPhone;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        IPhone iphone = new IPhone();
+
+        // Reprodutor Musical
+        iphone.criarPlaylist("Favoritas");
+        iphone.selecionar("Luiz Gonzaga");
+        iphone.reproduzir("O Cheiro Da Carolina");
+        iphone.pausar();
+
+        // Reprodutor de Vídeo
+        iphone.selecionar("Gladiador");
+        iphone.reproduzir("Gladiador");
+        iphone.selecionarLegenda("Português");        
+
+        // Navegador
+        iphone.exibirPagina("https://www.santander.com.br/");
+        iphone.adicionarNovaAba("https://dio.me");
+        iphone.atualizarPagina();
+
+        // Aparelho Telefônico
+        iphone.ligar("11999998888");
+        iphone.atender();
+        iphone.iniciarCorreioVoz();    
     }
 }
